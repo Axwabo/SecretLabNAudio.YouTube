80f3bc98770191316510944934d7332d1ffdd0c2
Axwabo <axwabo@gmail.com>
2026-03-14 16:40:57 +0100

    begin demo project

---
+++ SecretLabNAudio.YouTube.Demo/YouTubeDemoPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LabApi.Loader.Features.Plugins;
+
+namespace SecretLabNAudio.YouTube.Demo;
+
+public sealed class YouTubeDemoPlugin : Plugin
+{
+
+    public override string Name { get; }
+    public override string Description { get; }
+    public override string Author { get; }
+    public override Version Version { get; }
+    public override Version RequiredApiVersion { get; }
+
+    public override void Enable() => throw new NotImplementedException();
+
+    public override void Disable() => throw new NotImplementedException();
+
+}
