c979d651a87ca494f34e5cb881250e14600a0544
Axwabo <axwabo@gmail.com>
2026-04-30 11:17:51 +0200

    cached playlst items

--- SecretLabNAudio.YouTube/VideoIdPlaylistItem.cs
+++ SecretLabNAudio.YouTube/VideoIdPlaylistItem.cs
@@ -1,14 +1,19 @@
 using NAudio.Wave;
 using SecretLabNAudio.Core.Processors.Playlists;
+using SecretLabNAudio.FFmpeg.Caches;
 using SecretLabNAudio.FFmpeg.Processors;
+using SecretLabNAudio.YouTube.Caches;
 
 namespace SecretLabNAudio.YouTube;
 
 public sealed record VideoIdPlaylistItem(VideoId Id) : PlaylistItem
 {
 
+    internal static StreamBasedFFmpegAudioProcessor CreateProvider(VideoId videoId, int sampleRate, int channels)
+        => StreamBasedFFmpegAudioProcessor.Create(CreateYouTubeAudioProcessor.GetHighestQualityAsync(videoId), sampleRate, channels);
+
     public override ISampleProvider CreateProvider(int sampleRate, int channels)
-        => StreamBasedFFmpegAudioProcessor.Create(CreateYouTubeAudioProcessor.GetHighestQualityAsync(Id), sampleRate, channels);
+        => CreateProvider(Id, sampleRate, channels);
 
 }
 
@@ -16,6 +21,26 @@ public sealed record YouTubeVideoPlaylistItem(IVideo Video) : PlaylistItem(Video
 {
 
     public override ISampleProvider CreateProvider(int sampleRate, int channels)
-        => StreamBasedFFmpegAudioProcessor.Create(CreateYouTubeAudioProcessor.GetHighestQualityAsync(Video.Id), sampleRate, channels);
+        => VideoIdPlaylistItem.CreateProvider(Video.Id, sampleRate, channels);
+
+}
+
+public sealed record CachedVideoIdPlaylistItem(VideoId Id) : CachedPlaylistItem<VideoId, string>(Id)
+{
+
+    protected override AudioCacheBase<VideoId, string> GetCache() => YouTubeCache.Shared;
+
+    protected override ISampleProvider CreateFallback(int sampleRate, int channels)
+        => VideoIdPlaylistItem.CreateProvider(Id, sampleRate, channels);
+
+}
+
+public sealed record CachedYouTubeVideoPlaylistItem(IVideo Video) : CachedPlaylistItem<VideoId, string>(Video.Id, Video.Title)
+{
+
+    protected override AudioCacheBase<VideoId, string> GetCache() => YouTubeCache.Shared;
+
+    protected override ISampleProvider CreateFallback(int sampleRate, int channels)
+        => VideoIdPlaylistItem.CreateProvider(Video.Id, sampleRate, channels);
 
 }
